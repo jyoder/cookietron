14bea3317ab45ac5381f760d1a30d1883d6834a6
John Yoder <john.yoder@appfolio.com>
2019-11-28 13:42:08 -0800

    Update the main module to use the top-level API

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import Lib
+import Api (api, server)
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp
+import Protolude
+import Servant (serve)
+
+run :: IO ()
+run = do
+  let port = 3000
+      settings =
+        setPort port $ setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) defaultSettings
+  runSettings settings =<< mkApp
+
+mkApp :: IO Application
+mkApp = return $ serve api server
 
 main :: IO ()
-main = someFunc
+main = Main.run
