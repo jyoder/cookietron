787cda3f22250490a7ef31d1fe0b7ea157f18f65
John Yoder <john.yoder@appfolio.com>
2019-11-28 13:41:19 -0800

    Add a user API module

---
+++ src/UserApi.hs
@@ -0,0 +1,24 @@
+module UserApi (Api, server) where
+
+import qualified EmailAddress (example)
+import Id (Id (..))
+import Protolude
+import Servant
+import User (User (..))
+
+type Api =
+  "user" :> Get '[JSON] [User]
+    :<|> "user" :> Capture "userId" Integer :> Get '[JSON] User
+
+server :: Server Api
+server = getUsers :<|> getUserById
+
+getUsers :: Handler [User]
+getUsers = return [userExample]
+
+getUserById :: Integer -> Handler User
+getUserById 0 = return userExample
+getUserById _ = throwError err404
+
+userExample :: User
+userExample = User {id = Id 0, emailAddress = EmailAddress.example}
