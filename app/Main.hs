25e6164d74b705945e7a9feaa79ff15c90a42846
John Yoder <john.yoder@appfolio.com>
2019-12-28 10:21:09 -0800

    Refactor Main

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,21 @@ module Main where
 
 import Api (api, server)
 import Network.Wai (Application)
-import Network.Wai.Handler.Warp
+import Network.Wai.Handler.Warp (Settings, defaultSettings, runSettings, setBeforeMainLoop, setPort)
 import Protolude
 import Servant (serve)
 
-run :: IO ()
-run = do
-  let port = 3000
-      settings =
-        setPort port $ setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) defaultSettings
-  runSettings settings =<< mkApp
-
-mkApp :: IO Application
-mkApp = return $ serve api server
-
 main :: IO ()
-main = Main.run
+main = makeApp >>= runSettings (makeSettings defaultPort)
+
+makeApp :: IO Application
+makeApp = return $ serve api server
+
+makeSettings :: Int -> Settings
+makeSettings port = setPort port $ setBeforeMainLoop (showStartupMessage port) defaultSettings
+
+showStartupMessage :: Int -> IO ()
+showStartupMessage port = hPutStrLn stderr $ "Listening on port " ++ show port
+
+defaultPort :: Int
+defaultPort = 3000
